3ff442dba04bbaceba5238d49d284fc8560e81f8
Dimitar Jordanov <theshadowbyte@gmail.com>
2021-06-04 20:19:08 -0400

    Route slug restructuring

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -38,7 +39,7 @@ class Post {
      * @return Collection
      * @throws Exception
      */
-    public static function all() {
+    public static function all(): Collection {
 
         return cache()->rememberForever('posts.all', function () {
             return collect(File::files(resource_path("posts")))
@@ -64,6 +65,24 @@ class Post {
     public static function find( $slug ) {
 
         return static::all()->firstWhere('slug', $slug);
+
+    }
+
+    /**
+     * @param $slug
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public static function findOrFail( $slug ) {
+
+        $post = static::find($slug);
+
+        if ( !$post ) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Models\Post;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +15,23 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+
     return view('home');
+
 });
 
 Route::get('/posts', function () {
+
     return view('posts', [
         'posts' => Post::all()
     ]);
+
 });
 
 Route::get('posts/{post}', function ($slug) {
+
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => Post::findOrFail($slug)
     ]);
-})->where('post', '[A-z_\-]+');
+
+});
